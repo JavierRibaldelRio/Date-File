6935c48be47cc86378476f8603743546d7cd87c3
JavierRibaldelRio <javierribal@gmail.com>
2023-04-07 16:55:46 +0200

    Versión 2

--- date-file.sh
+++ date-file.sh
@@ -1,7 +1,17 @@
 #! /bin/sh
 
-read -p "Filename: " name
-
+# Obtiene la fecha
 now=$(date '+%Y-%m-%d' )
 
-touch "$now-$name"
+# Comprueba si hay un parametro
+if [ -z "$1" ]
+then
+    # Obtiene el nombre del archivo
+    read -p "Filename: " name
+
+    # crea el archivo
+    touch "$now-$name"
+
+else
+    touch "$now-$1"
+fi
